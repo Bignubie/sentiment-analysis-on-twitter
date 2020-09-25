9ba664fc92b7798250ce5f81a96b34ef43bb626a
Prakhar Dwivedi <52011660+meowmeow321@users.noreply.github.com>
2020-09-25 13:49:00 +0530

    Update gettweets.py
    Removed utf-8 and added pickle code

--- gettweets.py
+++ gettweets.py
@@ -1,4 +1,5 @@
 import tweepy
+import pickle
 
 twitter_keys = {
         'consumer_key':        'Jz6tdLX58ljCP8slSdYmrVnaV',
@@ -18,6 +19,13 @@ hashtag = input("Enter hashtag to search:")
 hashtag = "#" + hashtag
 
 cnt = int(input("Enter number of tweets to retrieve:"))
+tweets = []
 
 for tweet in tweepy.Cursor(api.search,q=hashtag,lang="en").items(cnt):
-    output_file.write(unicode(tweet.text).encode("utf-8") + '\n')
+    output_file.write(tweet.text+'\n')
+    tweets.append(tweet.text)
+
+output_file.close()
+output_file = open("tweetdump","wb")
+pickle.dump(tweets,output_file)
+print(str(len(tweets)) + " tweets collected!")
