4bb39ba434d3914a3f07421f8bb0b2b76ab4d56a
Ishan Gupta <44469233+Bignubie@users.noreply.github.com>
2020-09-25 15:00:05 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,16 @@ import tweepy
 import pickle
 import re
 
+def tweet_cleaner(text):
+    #remove @mentions, RT symbols, urls, extra chars, convert to lowercase and strip
+    noRT = re.sub(r'^[RT]+','',text)
+    nomention = re.sub(r'@[A-Za-z0-9_:]+','',noRT)
+    nourl = re.sub('https?://[A-Za-z0-9./]+','',nomention)
+    alphanum_only = re.sub(r'[^a-zA-Z0-9 ]+', '',nourl)
+    stripped = alphanum_only.lower().strip()
+    res = re.sub(' +', ' ', stripped)
+    return res
+
 twitter_keys = {
         'consumer_key':        'Jz6tdLX58ljCP8slSdYmrVnaV',
         'consumer_secret':     'syS20YGPenEifTtcZW3l7hRsR1DchlTjby88VhiBC50SAqQY5F',
@@ -12,42 +22,25 @@ twitter_keys = {
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(twitter_keys['consumer_key'],twitter_keys['consumer_secret'])
 auth.set_access_token(twitter_keys['access_token_key'],twitter_keys['access_token_secret'])
-
+    
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
-output_file = open("tweets.txt","w")
+query = input("Enter hashtag to search: ")
+cnt = int(input("Enter number of tweets to retrieve: "))
 
-hashtag = "#pop"
-cnt = 10
+query = "#" + query
 tweets = []
 
+for tweet in tweepy.Cursor(api.search,q=query,lang="en",tweet_mode='extended').items(cnt):
+    output_file.write(tweet.full_text+'\n')
+    tweets.append(tweet.full_text)
+    print(tweet.full_text)
 
-for tweet in tweepy.Cursor(api.user_timeline,id="realDonaldTrump").items(cnt):
-    tweets.append(tweet.text)
+print("\n UPDATED : \n")
     
-
-for t in tweets:
-    print(t)
-    
-    
-def tweet_cleaner(text):
-    #remove @mentions, RT symbols, urls, extra chars, convert to lowercase and strip
-    noRT = re.sub(r'^[RT]+','',text)
-    nomention = re.sub(r'@[A-Za-z0-9_:]+','',noRT)
-    nourl = re.sub('https?://[A-Za-z0-9./]+','',nomention)
-    alphanum_only = re.sub(r'[^a-zA-Z0-9 ]+', '',nourl)
-    stripped = alphanum_only.lower().strip()
-    return stripped
-
 ctweets = []
 for tweet in tweets:
     ctweet = tweet_cleaner(tweet)
     if len(ctweet)>5:
          ctweets.append(ctweet)
-            
-print(" ")        
-print("UPDATED :")
-print(" ")
-    
-for t in ctweets:
-    print(t)
+    print(ctweet)
