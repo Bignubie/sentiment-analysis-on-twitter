206967e0e43a0c467c621efc36a2c42112031694
Prakhar Dwivedi <52011660+meowmeow321@users.noreply.github.com>
2020-09-25 13:45:25 +0530

    Create cleanup.py

---
+++ cleanup.py
@@ -0,0 +1,23 @@
+import pickle
+import re
+
+tweets = pickle.load(open("tweetdump","rb"))
+#tweets.append("RT dndsdgi 12212 sfashHHj http://sfsf.com.s.s./s/s.apsx @cat")
+print(tweets)
+
+def tweet_cleaner(text):
+    #remove @mentions, RT symbols, urls, extra chars, convert to lowercase and strip
+    noRT = re.sub(r'^[RT]+','',text)
+    nomention = re.sub(r'@[A-Za-z0-9_:]+','',noRT)
+    nourl = re.sub('https?://[A-Za-z0-9./]+','',nomention)
+    alphanum_only = re.sub("[^a-zA-Z0-9]", " ",nourl)
+    stripped = alphanum_only.lower().strip()
+    return stripped
+
+ctweets = []
+for tweet in tweets:
+    ctweet = tweet_cleaner(tweet)
+    if len(ctweet)>5:
+         ctweets.append(ctweet)
+print(len(ctweets))
+print(ctweets)
